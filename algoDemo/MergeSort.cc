77de37635019de6594857e44b2e9dceb8e1555c8
David Patrzeba <david.patrzeba@gmail.com>
2013-01-21 19:31:13 -0500

    changed to use vector iterators

--- algoDemo/MergeSort.cc
+++ algoDemo/MergeSort.cc
@@ -40,16 +40,61 @@ void merge(vector<int> &, vector<int> &, unsigned, unsigned, unsigned);
 void sort(vector<int> &, vector<int> &, unsigned, unsigned);
 void sort(vector<int> &, unsigned, unsigned);
 
+void sort(vector<int>::iterator, vector<int>::iterator);
+void merge(vector<int>::iterator, vector<int>::iterator, vector<int>::iterator);
+
 int main(int argc, char **argv)
 {
 	vector<int> arr {20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
-	sort(arr, 0, 19);
+	sort(arr.begin(), arr.end() - 1);
 
-	for (const auto i : arr)
+	for (const auto i : arr) //type: const int
 		cout << i << " ";
 	cout << endl;
 
+	arr = {20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
+	sort(arr, 0, 19);
+
+	for (const auto i : arr) //type: const int
+		cout << i << " ";
+	cout << endl;
+
+
 	return 0;
+}
+
+void sort(vector<int>::iterator begin, vector<int>::iterator end)
+{
+	if (begin < end) {
+		vector<int>::iterator middle = (end - begin) / 2 + begin;
+		sort(begin, middle);
+		sort(middle + 1, end);
+		merge(begin, middle + 1, end);
+	}
+}
+
+void merge(vector<int>::iterator begin, vector<int>::iterator middle, vector<int>::iterator end)
+{
+	vector<int> l_vec (begin, middle);
+	vector<int> r_vec (middle, end + 1);
+	vector<int>::iterator l_iter (l_vec.begin());
+	vector<int>::iterator l_iter_e (l_vec.end());
+	vector<int>::iterator r_iter (r_vec.begin());
+	vector<int>::iterator r_iter_e (r_vec.end());
+
+	while (l_iter != l_iter_e && r_iter != r_iter_e) {
+		if (*l_iter < *r_iter)
+			*begin++ = *l_iter++;
+		else
+			*begin++ = *r_iter++;
+	}
+
+	while (l_iter != l_iter_e)
+		*begin++ = *l_iter++;
+
+	while(r_iter != r_iter_e)
+		*begin++ = *r_iter++; 
+
 }
 
 void sort(vector<int> &arr, unsigned start, unsigned end)
